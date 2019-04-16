ad13e38e6db8a2c9709c9aa0279ca638ce59e9e0
fh <frederikhestvang@gmail.com>
2019-04-16 10:14:05 +0200

    added loglive scripts

---
+++ ll.sh
@@ -0,0 +1,19 @@
+watchit() {
+    HOME=$(tput cup 0 0)
+    ED=$(tput ed)
+    EL=$(tput el)
+    ROWS=$(tput lines)
+    COLS=$(tput cols)
+    printf '%s%s' "$HOME" "$ED"
+    while true
+    do
+        CMD="$@"
+        ${SHELL:=sh} -c "$CMD" | head -n $ROWS | while IFS= read LINE; do
+            printf '%-*.*s%s\n' $COLS $COLS "$LINE" "$EL"
+        done
+        printf '%s%s' "$ED" "$HOME"
+        sleep 1
+    done
+}
+
+watchit top -b -n 1
